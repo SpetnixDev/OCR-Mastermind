a89cd5b3900b658c2ee3dbea384096c9d08486f1
Spetnix <spetnix@gmail.com>
2018-11-09 22:52:48 +0100

    Add MoreLess game

---
+++ src/net/spetnix/Main.java
@@ -0,0 +1,9 @@
+package net.spetnix;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MoreLess ml = new MoreLess(10);
+        ml.run();
+    }
+}
---
+++ src/net/spetnix/Mastermind.java
@@ -0,0 +1,11 @@
+package net.spetnix;
+
+public class Mastermind {
+    int rounds;
+
+    public Mastermind(int rounds) {
+        this.rounds = rounds;
+    }
+
+
+}
